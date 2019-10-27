ecec75d213c0188c5a3eed71804c42f54bb8888a
Musa Hamwala II <musahamwala@icloud.com>
2019-10-27 03:29:19 +0000

    Updates

---
+++ precache-manifest.7d3100f447abb35fb5dd0e2b87973d61.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3236e3d8f26db93f509c5a21b065b9ba",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a7002efb040349525930",
+    "url": "/react-portfolio/static/css/main.465e1dfd.chunk.css"
+  },
+  {
+    "revision": "405330444945a6beaf47",
+    "url": "/react-portfolio/static/js/2.3f350e85.chunk.js"
+  },
+  {
+    "revision": "a7002efb040349525930",
+    "url": "/react-portfolio/static/js/main.8f13e423.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "22d99701914fcbea5d218ea7a2e297ed",
+    "url": "/react-portfolio/static/media/main_pic.22d99701.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/react-portfolio/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/react-portfolio/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/react-portfolio/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/react-portfolio/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
