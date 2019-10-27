3c7ca8dbf821177f5c1048106a58a0e14cfa3737
Musa Hamwala II <musahamwala@icloud.com>
2019-10-27 13:24:28 +0000

    Updates

---
+++ precache-manifest.5870990482af8ac3c2a37589baa12abb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aba2584d88aad4d39ab88ea85df1a2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e199605225856dfabaf",
+    "url": "/static/css/main.dd59dffa.chunk.css"
+  },
+  {
+    "revision": "d3d2cc3504d66bbabd17",
+    "url": "/static/js/2.e3261184.chunk.js"
+  },
+  {
+    "revision": "4e199605225856dfabaf",
+    "url": "/static/js/main.7fc97fbd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b4aede459e7b2610128cd358931e761f",
+    "url": "/static/media/main_pic_2.b4aede45.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.5f2881baef248c363aeee6320273d82d.js"
+  "/precache-manifest.5870990482af8ac3c2a37589baa12abb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
